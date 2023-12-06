1d4ca7dbd22a9cb08fbd3b733deb148360510301
Algernon Soriano <alger.soriano@gmail.com>
2023-12-06 12:33:44 +1000

    badge

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    badges = []
+    num = 1
+    for name in names:
+        badges.append(f"Hello, {name}! You'll be assigned to room {num}!")
+        num += 1
+    return badges
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+
+    for name in assign_rooms(names):
+        print(name)
+    
+    return
+    
